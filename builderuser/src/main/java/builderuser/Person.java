be1c74cee36df31b33bf34d9e8d1eb16db4d1c9a
Pieed <Pieed@192.168.0.59>
2018-07-02 20:46:30 +0200

    builderUser added

---
+++ builderuser/src/main/java/builderuser/Person.java
@@ -0,0 +1,26 @@
+package builderuser;
+
+import annotationTest.MyBuilder;
+
+public class Person {
+	private String name;
+	private int alter;
+
+	public String getName() {
+		return name;
+	}
+
+	@MyBuilder
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAlter() {
+		return alter;
+	}
+
+	@MyBuilder
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+}
